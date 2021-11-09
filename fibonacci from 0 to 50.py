9cdda22ba108206fd7bf7415f6e0e528e10d740f
naveensek <93082058+naveensek@users.noreply.github.com>
2021-11-09 18:32:37 +0530

    Add files via upload

---
+++ fibonacci from 0 to 50.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[23]:
+
+
+first_num,next_num=0,1
+while next_num<=50:
+    print(next_num)
+    first_num,next_num=next_num,first_num+next_num
+        
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
